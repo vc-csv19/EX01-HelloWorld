2a1c5038acd21fa676d75822ba1a40242d96aa41
Allan Knight <44447314+vcdrk@users.noreply.github.com>
2020-01-26 22:41:35 -0800

    Update qemu-ubuntu.bat

--- qemu-ubuntu.bat
+++ qemu-ubuntu.bat
@@ -1,14 +1,13 @@
-qemu-system-aarch64^
+"C:\Program File\qemu\qemu-system-aarch64"^
   -cpu cortex-a57^
   -device rtl8139,netdev=net0^
   -device virtio-scsi-device^
-  -device scsi-cd,drive=cdrom^
   -device virtio-blk-device,drive=hd0^
-  -drive "if=none,file=C:\vc-csv19-dev\ubuntu-18.04.3-server-arm64.img.snapshot.qcow2,id=hd0"^
+  -drive "if=none,file=C:\vc-csv19-dev\ubuntu-18.04.3-arm64.img.shrunk.qcow2,id=hd0"^
   -m 2G^
   -machine virt^
   -netdev user,id=net0,hostfwd=tcp::5022-:22^
   -nographic^
-  -pflash "C:\vc-csv19\ubuntu-18.04.3-server-arm64-flash0.img"^
-  -pflash "C:\vc-csv19\ubuntu-18.04.3-server-arm64-flash1.img"^
+  -pflash "C:\vc-csv19-dev\ubuntu-18.04.3-arm64-flash0.img"^
+  -pflash "C:\vc-csv19-dev\ubuntu-18.04.3-arm64-flash1.img"^
   -smp 2
